1e913a3678c783065cc6947f88b54f9b5c6b9638
Jose Luis Díaz Alonso <134778793+JoseLuisDiazDev@users.noreply.github.com>
2024-08-18 17:57:55 +0200

    Hemos dado funcionalidad al efecto de ir cambiando texto de la imágen.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 
+/**Creamos una función que permita mostrar los diferentes textos en intervalos de x tiempo. */
 
+const texts = ["TRABAJAMOS PARA DEFENDER TUS DERECHOS", "HONESTIDAD Y ÉTICA PROFRESIONAL"]; //Array con los textos a mostrar.
+let index = 0; //Indice actual del texto.
+
+function changeText() {
+    document.getElementById('changingText').innerHTML = texts[index];
+    index = (index + 1) % texts.length; //Avanza al siguiente texto circularmente.
+
+}
+
+setInterval(changeText, 3000); //Cambia el texto cada 3000 milisegundos = 3 segundos.
